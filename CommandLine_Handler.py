2828ef52c523a65b0fdf472f41dc3749d3a76958
Oscar7532 <148721628+Oscar7532@users.noreply.github.com>
2023-10-24 14:36:05 +0100

    Update CommandLine_Handler.py

--- CommandLine_Handler.py
+++ CommandLine_Handler.py
@@ -5,4 +5,20 @@ def display(input):
         clo.output(input)
     except:
         eh.error("c-e20")
-
+        
+def login()
+    username = admin
+    password = 1234
+    if username == input("Username: ") and password == input("Password: "):
+        return True
+    return False
+    
+def interface(run_Interface = True):
+    while run_Interface == True:
+        if login():
+            clo.output("login successful")
+            while run_Interface == True:
+            
+            
+            
+        
